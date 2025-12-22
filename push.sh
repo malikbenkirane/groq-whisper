e643242a7b296529840d5a70ecd2bb40b5cff52e
Malik Benkirane <mbenkirane@internetworks.blog>
2025-12-22 17:05:59 +0100

    workaround(bucket.Push): sh script
    Maybe the issue comes from lookpath for gcloud resolving to gcloud.cmd
    instead of gcloud. Another mystery to Msys2 posix interface layer and os
    package compatibility issue.

---
+++ push.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# workaround for ./groq-whisper.exe dev build --push
+VERSION=$(./groq-whisper.exe version)
+./groq-whisper.exe dev build && \
+				$HOME/google-cloud-sdk/gcloud cp ./groq-$VERSION gs://groq-whisper && \
+				$HOME/google-cloud-sdk/gcloud cp ./setup/groq-setup-$VERSION gs://groq-whisper && \
