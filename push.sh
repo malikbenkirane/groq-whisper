91d95d79dff1e75a85cc63fb2ba580b2e4bec716
Malik Benkirane <mbenkirane@internetworks.blog>
2025-12-22 17:12:08 +0100

    fix(push.sh)

--- push.sh
+++ push.sh
@@ -2,8 +2,7 @@
 
 # workaround for ./groq-whisper.exe dev build --push
 VERSION=$(./groq-whisper.exe version)
-./groq-whisper.exe dev buil
 echo pushing $VERSION
 ./groq-whisper.exe dev build && \
-				$HOME/google-cloud-sdk/gcloud cp ./groq-$VERSION gs://groq-whisper && \
-				$HOME/google-cloud-sdk/gcloud cp ./setup/groq-setup-$VERSION gs://groq-whisper
+				gcloud cp ./groq-$VERSION gs://groq-whisper && \
+				gcloud cp ./setup/groq-setup-$VERSION gs://groq-whisper
