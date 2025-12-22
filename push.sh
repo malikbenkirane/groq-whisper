12ceb41f1ef2d701ea42d26e6d205a3f7cd88fe3
Malik Benkirane <mbenkirane@internetworks.blog>
2025-12-22 18:28:26 +0100

    fix push.sh

--- push.sh
+++ push.sh
@@ -1,8 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # workaround for ./groq-whisper.exe dev build --push
+go build
 VERSION=$(./groq-whisper.exe version)
 echo pushing $VERSION
 ./groq-whisper.exe dev build && \
-				gcloud storage cp ./groq-$VERSION gs://groq-whisper && \
-				gcloud storage cp ./setup/groq-setup-$VERSION gs://groq-whisper
+				gcloud storage cp groq-$VERSION.exe gs://groq-whisper && \
+				gcloud storage cp setup/groq-setup-$VERSION.exe gs://groq-whisper
