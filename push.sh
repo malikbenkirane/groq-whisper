bcd9708114e25f0db37b22d738e73dbd8522c6fa
Malik Benkirane <mbenkirane@internetworks.blog>
2025-12-22 21:40:08 +0100

    v0.8.0 fix push.sh to push exe files
    Currently unzip of binaries is not supported by the installer/upgrader.

--- push.sh
+++ push.sh
@@ -7,4 +7,6 @@ VERSION=$(./groq-whisper.exe version)
 echo pushing $VERSION
 ./groq-whisper.exe dev build && \
 	./groq-whisper.exe dev zip && \
-	gcloud storage cp groq-setup-$VERSION.zip gs://groq-whisper
+	gcloud storage cp groq-setup-$VERSION.zip gs://groq-whisper && \
+	gcloud storage cp setup/groq-setup-$VERSION.exe gs://groq-whisper && \
+	gcloud storage cp groq-setup-$VERSION.exe gs://groq-whisper
