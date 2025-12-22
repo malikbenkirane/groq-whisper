dffebc77e2ceba6e50cccea5e38b97f3f4292157
Malik Benkirane <mbenkirane@internetworks.blog>
2025-12-22 14:39:32 +0100

    refactor(setup): reorganize installer code into separate files
    The dependency installation logic was consolidated in a single
    `downloader.go` file, mixing extraction, installer interface
    definitions, PortAudio configuration, and download orchestration.
    This made the codebase difficult to navigate and violated single
    responsibility principle.
    
    Reorganizes code across three focused files:
    
    **installer.go** (new):
    - Moves `Installer` interface and implementation from downloader.go
    - Contains `NewInstaller()`, config types, and functional options
    - Houses `extract()` method for 7z file processing
    - Implements `Install()` method as public API (renamed from `Download()`)
    
    **resources.go**:
    - Relocates `DefaultPortAudioDst()` from downloader.go
    - Adds `PortAudioDst` type definition (previously inline)
    - Groups all PortAudio-related configuration in single file
    
    **downloader.go**:
    - Retains only `download()` method implementation
    - Now focused solely on fetching and extracting dependencies
    - Removes progress bar clear-on-finish option per commit 1be1d34
    
    **cmd/install_deps.go**:
    - Updates method call from `Download()` to `Install()` to reflect
      actual behavior (installs both ffmpeg and PortAudio DLL)
    - Changes default installation path from "groq-whisper" to
      "groq-deps" for clarity
    
    This continues the installer implementation mentioned in commits
    34b5a5d and 1be1d34, separating concerns in preparation for
    completing the Installer interface with additional dependency types.
    
    No functional changes to extraction or installation logic.

--- setup/pkg/dep/downloader.go
+++ setup/pkg/dep/downloader.go
@@ -2,69 +2,16 @@ package dep
 
 import (
 	"bytes"
-	"errors"
 	"fmt"
 	"io"
+	"net/http"
 	"os"
-	"path"
-	"strings"
 
 	"github.com/bodgit/sevenzip"
 	"github.com/schollz/progressbar/v3"
 
 	"github.com/malikbenkirane/groq-whisper/setup/pkg/dcheck"
-	"github.com/malikbenkirane/groq-whisper/setup/pkg/gcloud"
 )
-
-func NewInstaller(path, bucket, ffmpeg7z string, opts ...InstallerOption) (Installer, error) {
-	conf := InstallerConfig{}
-	for _, opt := range opts {
-		conf = opt(conf)
-	}
-	pdst, err := DefaultPortAudioDst()
-	if err != nil {
-		return nil, fmt.Errorf("default port audio dest: %w", err)
-	}
-	return &installer{
-		pdst:     pdst,
-		psrc:     DefaultPortAudioSrc(),
-		bucket:   gcloud.NewBucket(bucket),
-		ffmpeg7z: ffmpeg7z,
-		path:     path,
-		conf:     conf,
-	}, nil
-}
-
-type Installer interface {
-	Download() error
-}
-
-type InstallerConfig struct {
-	overwrite bool
-}
-
-type InstallerOption func(InstallerConfig) InstallerConfig
-
-func InstallerWithOverwrite() InstallerOption {
-	return func(ic InstallerConfig) InstallerConfig {
-		ic.overwrite = true
-		return ic
-	}
-}
-
-type installer struct {
-	psrc     PortAudioSrc
-	pdst     *PortAudioDst
-	bucket   gcloud.Bucket
-	ffmpeg7z string
-	path     string
-
-	conf InstallerConfig
-}
-
-func (i installer) Download() error {
-	return i.download()
-}
 
 func (i installer) download() (err error) {
 	if i.conf.overwrite {
@@ -86,7 +33,6 @@ func (i installer) download() (err error) {
 			}
 			bar := progressbar.NewOptions(len(reader.File),
 				progressbar.OptionShowDescriptionAtLineEnd(),
-				progressbar.OptionClearOnFinish(),
 				progressbar.OptionSetDescription("install ffmpeg"),
 				progressbar.OptionSetMaxDetailRow(8))
 			for _, f := range reader.File {
@@ -95,69 +41,31 @@ func (i installer) download() (err error) {
 					return fmt.Errorf("extract: %w", err)
 				}
 				if err := bar.AddDetail(dst); err != nil {
+					return fmt.Errorf("bar add detail: %w", err)
+				}
+				if err := bar.Add(1); err != nil {
 					return fmt.Errorf("bar add: %w", err)
 				}
 			}
 		}
 	}
+	{
+		dst := i.pdst.path
+		f, err := os.Create(dst)
+		defer func() {
+			err = dcheck.Wrap(f.Close(), err, "close %q", dst)
+		}()
+		url := i.psrc.Dll64Url
+		resp, err := http.Get(url)
+		if err != nil {
+			return fmt.Errorf("http get %q: %w", url, err)
+		}
+		defer func() {
+			err = dcheck.Wrap(resp.Body.Close(), err, "close body")
+		}()
+		if _, err = io.Copy(f, resp.Body); err != nil {
+			return fmt.Errorf("copy to %q: %w", dst, err)
+		}
+	}
 	return nil
-}
-
-func (i installer) extract(fz *sevenzip.File) (dst string, err error) {
-	rc, err := fz.Open()
-	if err != nil {
-		return "", fmt.Errorf("7z open %q: %w", fz.Name, err)
-	}
-	defer func() {
-		err = dcheck.Wrap(rc.Close(), err, "close %q", fz.Name)
-	}()
-	parts := strings.Split(fz.Name, "/")
-	parts[0] = i.path
-	dst = path.Join(parts...)
-	_, err = os.Stat(dst)
-	if !errors.Is(err, os.ErrNotExist) {
-		if !i.conf.overwrite {
-			return dst, nil
-		}
-	}
-	if err != nil {
-		return "", fmt.Errorf("stat %q: %w", dst, err)
-	}
-	if parts[len(parts)-1] == "" {
-		if err := os.MkdirAll(dst, 0700); err != nil {
-			return "", fmt.Errorf("mkdir %q: %w", dst, err)
-		}
-		return dst, nil
-	}
-	f, err := os.Create(dst)
-	if err != nil {
-		return "", fmt.Errorf("create %q: %w", dst, err)
-	}
-	defer func() {
-		err = dcheck.Wrap(f.Close(), err, "close %q", dst)
-	}()
-	_, err = io.Copy(f, rc)
-	if err != nil {
-		return "", fmt.Errorf("install to %q: %w", dst, err)
-	}
-	return dst, nil
-}
-
-func DefaultPortAudioDst() (*PortAudioDst, error) {
-	home, err := os.UserHomeDir()
-	if err != nil {
-		return nil, fmt.Errorf("user home dir: %w", err)
-	}
-	parts := strings.Split(home, ":")
-	if len(parts) != 2 {
-		return nil, fmt.Errorf("unable to extract volume from %q", home)
-	}
-	volume := parts[0]
-	return &PortAudioDst{
-		path: path.Join(volume, "Windows", "System32", "libportaudio.dll"),
-	}, nil
-}
-
-type PortAudioDst struct {
-	path string
 }
---
+++ setup/pkg/dep/installer.go
@@ -0,0 +1,109 @@
+package dep
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"path"
+	"strings"
+
+	"github.com/bodgit/sevenzip"
+	"github.com/malikbenkirane/groq-whisper/setup/pkg/dcheck"
+	"github.com/malikbenkirane/groq-whisper/setup/pkg/gcloud"
+)
+
+func NewInstaller(path, bucket, ffmpeg7z string, opts ...InstallerOption) (Installer, error) {
+	conf := InstallerConfig{}
+	for _, opt := range opts {
+		conf = opt(conf)
+	}
+	pdst, err := DefaultPortAudioDst()
+	if err != nil {
+		return nil, fmt.Errorf("default port audio dest: %w", err)
+	}
+	return &installer{
+		pdst:     pdst,
+		psrc:     DefaultPortAudioSrc(),
+		bucket:   gcloud.NewBucket(bucket),
+		ffmpeg7z: ffmpeg7z,
+		path:     path,
+		conf:     conf,
+	}, nil
+}
+
+type Installer interface {
+	Install() error
+}
+
+type InstallerConfig struct {
+	overwrite bool
+}
+
+type InstallerOption func(InstallerConfig) InstallerConfig
+
+func InstallerWithOverwrite() InstallerOption {
+	return func(ic InstallerConfig) InstallerConfig {
+		ic.overwrite = true
+		return ic
+	}
+}
+
+type installer struct {
+	psrc     PortAudioSrc
+	pdst     *PortAudioDst
+	bucket   gcloud.Bucket
+	ffmpeg7z string
+	path     string
+
+	conf InstallerConfig
+}
+
+func (i installer) Install() error {
+	// curently there is no clear separation of concerns and the name
+	// "download" is not appropriate as we overwrite portaudio.dll here
+	if err := i.download(); err != nil {
+		return fmt.Errorf("download: %w", err)
+	}
+	return nil
+}
+
+func (i installer) extract(fz *sevenzip.File) (dst string, err error) {
+	rc, err := fz.Open()
+	if err != nil {
+		return "", fmt.Errorf("7z open %q: %w", fz.Name, err)
+	}
+	defer func() {
+		err = dcheck.Wrap(rc.Close(), err, "close %q", fz.Name)
+	}()
+	parts := strings.Split(fz.Name, "/")
+	parts[0] = i.path
+	dst = path.Join(parts...)
+	_, err = os.Stat(dst)
+	if !errors.Is(err, os.ErrNotExist) {
+		if !i.conf.overwrite {
+			return dst, nil
+		}
+	}
+	if err != nil {
+		return "", fmt.Errorf("stat %q: %w", dst, err)
+	}
+	if parts[len(parts)-1] == "" {
+		if err := os.MkdirAll(dst, 0700); err != nil {
+			return "", fmt.Errorf("mkdir %q: %w", dst, err)
+		}
+		return dst, nil
+	}
+	f, err := os.Create(dst)
+	if err != nil {
+		return "", fmt.Errorf("create %q: %w", dst, err)
+	}
+	defer func() {
+		err = dcheck.Wrap(f.Close(), err, "close %q", dst)
+	}()
+	_, err = io.Copy(f, rc)
+	if err != nil {
+		return "", fmt.Errorf("install to %q: %w", dst, err)
+	}
+	return dst, nil
+}
