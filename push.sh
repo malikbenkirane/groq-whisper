a18e94c20c76b476a3d355b4b9efe7f18dac7ca8
Malik Benkirane <mbenkirane@internetworks.blog>
2025-12-22 17:09:46 +0100

    fix(push.sh): incorrect file
    add echo version and build step

--- push.sh
+++ push.sh
@@ -2,6 +2,8 @@
 
 # workaround for ./groq-whisper.exe dev build --push
 VERSION=$(./groq-whisper.exe version)
+./groq-whisper.exe dev buil
+echo pushing $VERSION
 ./groq-whisper.exe dev build && \
 				$HOME/google-cloud-sdk/gcloud cp ./groq-$VERSION gs://groq-whisper && \
-				$HOME/google-cloud-sdk/gcloud cp ./setup/groq-setup-$VERSION gs://groq-whisper && \
+				$HOME/google-cloud-sdk/gcloud cp ./setup/groq-setup-$VERSION gs://groq-whisper
