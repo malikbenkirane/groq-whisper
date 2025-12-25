027f8c27f696f5fb12f343e7f63e3744e1e22c77
Malik Benkirane <mbenkirane@internetworks.blog>
2025-12-25 10:34:58 +0100

    host-v handle post transcript omitted

---
+++ host/internal/adapter/rest/https/transcripts.go
@@ -0,0 +1,49 @@
+package https
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/malikbenkirane/groq-whisper/host/internal/domain/session"
+	"github.com/malikbenkirane/groq-whisper/host/internal/domain/transcript"
+	"github.com/malikbenkirane/groq-whisper/host/internal/repo"
+)
+
+func (a adapter) handlePostTranscript() customHandler {
+	return func(w http.ResponseWriter, r *http.Request) (errUser error, errSys error) {
+		if r.Header.Get("Content-Type") != "application/json" {
+			return errExpectedContentTypeJSON, fmt.Errorf(
+				"%w: got %q", errExpectedContentTypeJSON, r.Header.Get("Content-Type"))
+		}
+
+		var s session.Id
+		{
+			intId, err := strconv.Atoi(r.PathValue("session"))
+			if err != nil {
+				return errBadRequest, fmt.Errorf("%w: %w", errStrconvSession, err)
+			}
+			s = session.Id(intId)
+		}
+
+		var tx struct {
+			Tx string
+			Ts time.Time
+		}
+
+		if err := json.NewDecoder(r.Body).Decode(&tx); err != nil {
+			return errInternalError, fmt.Errorf("%w: %w: %w", errDecodeTxPayload, errJsonDecode, err)
+		}
+
+		if err := a.repo.SaveTranscriptChunk(transcript.Chunk{
+			Text:      tx.Tx,
+			Timestamp: tx.Ts,
+		}, s); err != nil {
+			return errInternalError, fmt.Errorf("%w: %w", repo.ErrSaveTranscriptChunk, err)
+		}
+
+		return
+	}
+}
